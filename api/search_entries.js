8e18d06044c9f96fa5fb9f9dc85e61c7987fa872
Rafał Majewski <goodheropl@gmail.com>
2022-01-08 19:42:48 +0100

    fix search entries

--- api/search_entries.js
+++ api/search_entries.js
@@ -11,7 +11,7 @@ const entriesManager = require("../modules/entriesManager.js");
 
 const router = {
 	[GET]: (req, res) => {
-		const {query} = req.getQuery() || {};
+		const {query} = req.getQueryParam("query");
 		if (query === undefined) {
 			res.setStatusCode(400).endText("Bad Request, no query provided");
 			return;
