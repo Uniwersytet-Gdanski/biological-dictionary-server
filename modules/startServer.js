b7d181815f58216ef0215ac164ba4692824a568a
Rafał Majewski <goodheropl@gmail.com>
2022-01-08 17:17:07 +0100

    access-control-allow-origin and logging requests

--- modules/startServer.js
+++ modules/startServer.js
@@ -1,9 +1,17 @@
 const workful = require("workful");
 const config = require("../config.json");
 const router = require("../router.js");
+const {logStdout} = require("../utils/timeLog.js");
+
+const startCallback = (req, res) => {
+	res.setHeader("access-control-allow-origin", "*");
+	logStdout(`${req.method} ${req.getPath()}`);
+};
 
 const startServer = () => (new Promise((resolve, reject) => {
-	const server = workful.createServer(router);
+	const server = workful.createServer(router, {
+		startCallback,
+	});
 	server.listen(config.port);
 	server.on("listening", () => {
 		resolve(server);
