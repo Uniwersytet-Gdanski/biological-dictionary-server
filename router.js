ebbca29f9c7e92a9cac74e0206f8c6f38394f4b1
Rafał Majewski <goodheropl@gmail.com>
2022-01-14 21:55:56 +0100

    fix cors

--- router.js
+++ router.js
@@ -1,10 +1,13 @@
+const workful = require("workful");
+
 const {
 	POST,
-} = require("workful").methodsSymbols;
+} = workful.methodsSymbols;
 
 const deployMiddleware = require("./modules/deployMiddleware.js");
 
 const router = [
+	workful.middlewares.cors,
 	deployMiddleware.get,
 	{
 		"api": require("./api/router.js"),
