2e52b3a41351bfeb6dfc8742d77cd27baf41bc60
Rafał Majewski <goodheropl@gmail.com>
2022-01-20 18:02:31 +0100

    enries by prefix doesnt return full entries by default

--- api/entries_by_prefix/router.js
+++ api/entries_by_prefix/router.js
@@ -1,23 +1,18 @@
 const workful = require("workful");
 const yup = require("yup");
 const paginate = require("../../src/modules/paginate.js");
+const entriesManager = require("../../src/modules/entriesManager.js");
 
+const {maxPageSize} = require("../../config.json");
 const {
 	GET,
 } = workful.methodsSymbols;
 
-const {maxPageSize} = require("../../config.json");
-
-const entriesManager = require("../../src/modules/entriesManager.js");
-
-
-
-
 const queryParamsSchema = yup.object().shape({
-	prefix: yup.string().required(),
+	prefix: yup.string().required().transform((value) => (value.toLowerCase())),
 	pageNumber: yup.number().integer().min(1).default(1),
 	pageSize: yup.number().integer().min(1).max(maxPageSize).default(10),
-	withoutFullEntries: yup.boolean().nullable().transform((value) => (value === null ? true : value)),
+	withFullEntries: yup.boolean().default(false),
 });
 
 const router = {
@@ -28,7 +23,7 @@ const router = {
 				prefix,
 				pageNumber,
 				pageSize,
-				withoutFullEntries: isWithoutFullEntries,
+				withFullEntries: isWithFullEntries,
 			} = await queryParamsSchema.validate(req.getQueryParams());
 			const entries = entriesManager.getAll().reduce((entries, entry) => {
 				for (const name of entry.names) {
@@ -36,7 +31,7 @@ const router = {
 					entries.push({
 						id: entry.id,
 						name: name,
-						...(!isWithoutFullEntries && {entry}),
+						...(!isWithFullEntries && {entry}),
 					});
 				}
 				return entries;
@@ -48,7 +43,7 @@ const router = {
 				pagesCount: Math.ceil(entries.length / pageSize),
 				data: entries.slice((pageNumber - 1) * pageSize, pageNumber * pageSize),
 			};
-		})
+		}),
 	],
 };
 
