b06abddee7978e25ffb39446211a9eb485205ddb
Rafał Majewski <goodheropl@gmail.com>
2022-01-20 18:02:52 +0100

    add return and async keywords

--- api/entries_first_letters/router.js
+++ api/entries_first_letters/router.js
@@ -4,18 +4,17 @@ const {
 	GET,
 } = workful.methodsSymbols;
 
-
 const entriesManager = require("../../src/modules/entriesManager.js");
 
 const router = {
-	[GET]: (req, res) => {
+	[GET]: async (req, res) => {
 		const firstLetters = entriesManager.getAll().reduce((firstLetters, entry) => {
 			for (const name of entry.names) {
 				firstLetters.add(name[0].toLowerCase());
 			}
 			return firstLetters;
 		}, new Set());
-		res.setStatusCode(200).endJson(
+		return res.setStatusCode(200).endJson(
 			Array.from(firstLetters).sort(
 				(firstLetter1, firstLetter2) => (firstLetter1.localeCompare(firstLetter2))
 			)
