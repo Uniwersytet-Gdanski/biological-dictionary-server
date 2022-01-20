d3c21aff76902adf21af65b070eb73c97aa22664
Rafał Majewski <goodheropl@gmail.com>
2022-01-20 18:04:09 +0100

    move calculating permutations to utils

--- src/modules/entriesManager.js
+++ src/modules/entriesManager.js
@@ -1,21 +1,11 @@
 const Entry = require("../../models/Entry.js");
 const latinize = require("latinize");
-
-
+const calculatePermutations = require("../utils/calculatePermutations.js");
 
 class Node {
 	nodeByCharacter = {};
 	matches = [];
 }
-
-const calculatePermutations = (array) => {
-	if (array.length == 0) return [];
-	if (array.length == 1) return [array];
-	return array.flatMap((element, i) => (
-		calculatePermutations(array.slice(0, i).concat(array.slice(i + 1))).map((permutation) => [element].concat(permutation))
-	));
-};
-
 
 class LookupTree {
 	#rootNode = new Node();
