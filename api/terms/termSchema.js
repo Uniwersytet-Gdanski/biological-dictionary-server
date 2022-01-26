232e8b70d5301c3016ccacde4c89313f99c2f841
Rafał Majewski <goodheropl@gmail.com>
2022-01-27 00:50:51 +0100

    require singular and plurar forms of translation to be nullable and defined

--- api/terms/termSchema.js
+++ api/terms/termSchema.js
@@ -4,8 +4,8 @@ const termSchema = yup.object().shape({
 	names: yup.array().of(yup.string().required()).required().min(1),
 	definition: yup.string().required(),
 	englishTranslations: yup.array().of(yup.object().shape({
-		singular: yup.string().required(),
-		plural: yup.string().required(),
+		singular: yup.string().nullable().defined(),
+		plural: yup.string().nullable().defined(),
 	})).required().min(1),
 });
 
